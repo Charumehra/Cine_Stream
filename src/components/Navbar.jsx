46eb65534f0c31935aabf42ef74f92e4f3347490
Charu Mehra <Charumehracm2004@gmail.com>
2026-04-17 17:39:07 +0530

    Refactor Navbar links for consistency

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,27 +9,41 @@ const Navbar = ({ query, setQuery }) => {
     <nav className="bg-gradient-to-r from-red-800 via-red-600 to-orange-600 shadow-lg">
       <div className="container mx-auto px-4">
         <div className="flex justify-between items-center h-16">
-          <Link to="/" className="text-2xl font-bold text-black tracking-widest cursor-pointer">
+          <Link
+            to="/"
+            className="text-2xl font-bold text-black tracking-widest cursor-pointer"
+          >
             Cine-Stream
           </Link>
 
           <div className="hidden md:flex space-x-8 items-center">
-            <Link to="/" className="text-black hover:text-blue-200 font-medium cursor-pointer">
+            <Link
+              to="/"
+              className="text-black hover:text-blue-200 font-medium cursor-pointer"
+            >
               Home
             </Link>
-            <Link to="/popular" className="text-black hover:text-blue-200 font-medium cursor-pointer">
+            <Link
+              to="/popular"
+              className="text-black hover:text-blue-200 font-medium cursor-pointer"
+            >
               Popular
             </Link>
-            <Link to="/top-rated" className="text-black hover:text-blue-200 font-medium cursor-pointer">
+            <Link
+              to="/top-rated"
+              className="text-black hover:text-blue-200 font-medium cursor-pointer"
+            >
               Top Rated
             </Link>
-            <Link to="/favourites" className="text-black hover:text-blue-200 font-medium cursor-pointer">
+            <Link
+              to="/favourites"
+              className="text-black hover:text-blue-200 font-medium cursor-pointer"
+            >
               Favourites
             </Link>
           </div>
 
           <div className="hidden md:flex items-center">
-           <Link to="/">
             <div className="relative">
               <input
                 type="text"
@@ -40,7 +54,6 @@ const Navbar = ({ query, setQuery }) => {
               />
               <Search className="absolute right-3 top-2 text-black w-5 h-5" />
             </div>
-           </Link>
           </div>
 
           <div className="md:hidden">
@@ -56,16 +69,28 @@ const Navbar = ({ query, setQuery }) => {
 
         {isOpen && (
           <div className="md:hidden pb-4 space-y-2">
-            <Link to="/" className="block px-4 py-2 text-black hover:bg-black hover:text-white transition-colors">
+            <Link
+              to="/"
+              className="block px-4 py-2 text-black hover:bg-black hover:text-white transition-colors"
+            >
               Home
             </Link>
-            <Link to="/popular" className="block px-4 py-2 text-black hover:bg-black hover:text-white transition-colors">
+            <Link
+              to="/popular"
+              className="block px-4 py-2 text-black hover:bg-black hover:text-white transition-colors"
+            >
               Popular
             </Link>
-            <Link to="/top-rated" className="block px-4 py-2 text-black hover:bg-black hover:text-white transition-colors">
+            <Link
+              to="/top-rated"
+              className="block px-4 py-2 text-black hover:bg-black hover:text-white transition-colors"
+            >
               Top Rated
             </Link>
-            <Link to="/favourites" className="block px-4 py-2 text-black hover:bg-black hover:text-white transition-colors">
+            <Link
+              to="/favourites"
+              className="block px-4 py-2 text-black hover:bg-black hover:text-white transition-colors"
+            >
               Favourites
             </Link>
 
